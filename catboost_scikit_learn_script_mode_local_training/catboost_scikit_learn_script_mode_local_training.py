faeac3eea0609ed63b0229a674699c7ac4479615
Eitan Sela <eitan.sela@gmail.com>
2021-06-04 10:48:20 +0300

    Added catboost_scikit_learn_script_mode_local_training sample code

---
+++ catboost_scikit_learn_script_mode_local_training/catboost_scikit_learn_script_mode_local_training.py
@@ -0,0 +1,80 @@
+# This is a sample Python program that trains a simple CatBoost model using SageMaker scikit-learn Docker image.
+# This implementation will work on your *local computer* or in the *AWS Cloud*.
+#
+# Prerequisites:
+#   1. Install required Python packages:
+#      `pip install -r requirements.txt`
+#   2. Docker Desktop installed and running on your computer:
+#      `docker ps`
+#   3. You should have AWS credentials configured on your local machine
+#      in order to be able to pull the docker image from ECR.
+###############################################################################################
+
+import itertools
+import pandas as pd
+import numpy as np
+import os
+
+from sagemaker.sklearn import SKLearn, SKLearnModel
+from sklearn import datasets
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+
+DUMMY_IAM_ROLE = 'arn:aws:iam::111111111111:role/service-role/AmazonSageMaker-ExecutionRole-20200101T000001'
+
+local_train = './data/train/boston_train.csv'
+local_validation = './data/validation/boston_validation.csv'
+
+def download_training_and_eval_data():
+    if os.path.isfile('./data/train/boston_train.csv') and \
+            os.path.isfile('./data/validation/boston_validation.csv'):
+        print('Training and dataset exist. Skipping Download')
+    else:
+        print('Downloading training dataset')
+
+        os.makedirs("./data", exist_ok=True)
+        os.makedirs("./data/train", exist_ok=True)
+        os.makedirs("./data/validation", exist_ok=True)
+
+        data = load_boston()
+
+        X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.25, random_state=45)
+        X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=45)
+
+        trainX = pd.DataFrame(X_train, columns=data.feature_names)
+        trainX['target'] = y_train
+
+        valX = pd.DataFrame(X_test, columns=data.feature_names)
+        valX['target'] = y_test
+
+        testX = pd.DataFrame(X_test, columns=data.feature_names)
+
+        trainX.to_csv(local_train, header=None, index=False)
+        valX.to_csv(local_validation, header=None, index=False)
+
+        print('Downloading completed')
+
+
+def main():
+    download_training_and_eval_data()
+
+    print('Starting model training.')
+    print('Note: if launching for the first time in local mode, container image download might take a few minutes to complete.')
+
+    sklearn = SKLearn(
+        entry_point="catboost_train.py",
+        source_dir='code',
+        framework_version="0.23-1",
+        instance_type="local",
+        role=DUMMY_IAM_ROLE,
+    )
+
+    train_location = 'file://' + local_train
+    validation_location = 'file://' + local_validation
+
+    sklearn.fit({'train': train_location, 'validation': validation_location})
+    print('Completed model training')
+
+
+if __name__ == "__main__":
+    main()
---
+++ catboost_scikit_learn_script_mode_local_training/code/catboost_train.py
@@ -0,0 +1,91 @@
+#  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License.
+#  A copy of the License is located at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  or in the "license" file accompanying this file. This file is distributed
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#  express or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+from __future__ import print_function
+
+import argparse
+import os
+
+import joblib
+import pandas as pd
+import numpy as np
+from catboost import CatBoostRegressor
+
+model_file_name = 'catboost-regressor-model.dump'
+
+
+if __name__ == "__main__":
+    print("Training Started")
+    parser = argparse.ArgumentParser()
+
+    # Sagemaker specific arguments. Defaults are set in the environment variables.
+    parser.add_argument("--output-data-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--train", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+
+    args = parser.parse_args()
+    print("Got Args: {}".format(args))
+
+    # Take the set of files and read them all into a single pandas dataframe
+    train_input_files = [os.path.join(args.train, file) for file in os.listdir(args.train)]
+    if len(train_input_files) == 0:
+        raise ValueError(
+            (
+                "There are no files in {}.\n"
+                + "This usually indicates that the channel ({}) was incorrectly specified,\n"
+                + "the data specification in S3 was incorrectly specified or the role specified\n"
+                + "does not have permission to access the data."
+            ).format(args.train, "train")
+        )
+    raw_data = [pd.read_csv(file, header=None, engine="python") for file in train_input_files]
+    train_df = pd.concat(raw_data)
+
+    validation_input_files = [os.path.join(args.train, file) for file in os.listdir(args.train)]
+    if len(validation_input_files) == 0:
+        raise ValueError(
+            (
+                    "There are no files in {}.\n"
+                    + "This usually indicates that the channel ({}) was incorrectly specified,\n"
+                    + "the data specification in S3 was incorrectly specified or the role specified\n"
+                    + "does not have permission to access the data."
+            ).format(args.train, "train")
+        )
+    raw_data = [pd.read_csv(file, header=None, engine="python") for file in validation_input_files]
+    validation_df = pd.concat(raw_data)
+
+    # Assumption is that the label is the last column
+    print('building training and validation datasets')
+    X_train = train_df.iloc[:, :-1].values
+    y_train = train_df.iloc[:, -1:].values
+    X_validation = validation_df.iloc[:, :-1].values
+    y_validation = validation_df.iloc[:, -1:].values
+
+    # define and train model
+    model = CatBoostRegressor()
+
+    model.fit(X_train, y_train, eval_set=(X_validation, y_validation), logging_level='Silent')
+
+    # print abs error
+    print('validating model')
+    abs_err = np.abs(model.predict(X_validation) - y_validation)
+
+    # print couple perf metrics
+    for q in [10, 50, 90]:
+        print('AE-at-' + str(q) + 'th-percentile: ' + str(np.percentile(a=abs_err, q=q)))
+
+    # persist model
+    path = os.path.join(args.model_dir, model_file_name)
+    print('saving model file to {}'.format(path))
+    model.save_model(path, format='json')
+
+    print("Training Completed")
