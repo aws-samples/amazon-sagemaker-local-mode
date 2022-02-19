3c12489d846177afa9160778d0985fcd78a8ba49
Eitan Sela <eitan.sela@gmail.com>
2022-02-19 23:36:17 +0200

    Initial push of gensim_with_word2vec_model_artifacts_local_serving sample code

---
+++ gensim_with_word2vec_model_artifacts_local_serving/code/inference.py
@@ -0,0 +1,29 @@
+import os
+import json
+from gensim.models import KeyedVectors
+
+def input_fn(request_body, request_content_type):
+    print(f"request_body: {request_body}")
+    print(type(request_body))
+    if request_content_type == "application/json":
+        payload = json.loads(request_body)
+        instances = payload["instances"]
+        return instances
+
+
+def predict_fn(instances, word_vectors):
+    #########################################
+    # Do your custom preprocessing logic here
+    #########################################
+
+    print(f"instances: {instances}")
+    print("calling model")
+    predictions = word_vectors.most_similar(positive=instances)
+    return predictions
+
+
+def model_fn(model_dir):
+    print("loading model from: {}".format(model_dir))
+    word_vectors = KeyedVectors.load_word2vec_format(os.path.join(model_dir, "vectors.txt"), binary=False)
+    print(f'word vectors length: {len(word_vectors)}')
+    return word_vectors
---
+++ gensim_with_word2vec_model_artifacts_local_serving/gensim_with_word2vec_model_artifacts_local_serving.py
@@ -0,0 +1,55 @@
+# This is a sample Python program that serve a Word2Vec model, trained with BlazingText algorithm with inference using gensim.
+# This implementation will work on your *local computer* or in the *AWS Cloud*.
+#
+# Prerequisites:
+#   1. Install required Python packages:
+#      `pip install -r requirements.txt`
+#   2. Docker Desktop installed and running on your computer:
+#      `docker ps`
+#   3. You should have AWS credentials configured on your local machine
+#      in order to be able to pull the docker image from ECR.
+###############################################################################################
+
+import boto3
+from sagemaker.deserializers import JSONDeserializer
+from sagemaker.serializers import JSONSerializer
+from sagemaker.sklearn import SKLearnModel
+
+
+DUMMY_IAM_ROLE = 'arn:aws:iam::111111111111:role/service-role/AmazonSageMaker-ExecutionRole-20200101T000001'
+s3 = boto3.client('s3')
+
+
+def main():
+
+    # Download a pre-trained model archive file
+    print('Downloading a pre-trained model archive file')
+    s3.download_file('aws-ml-blog', 'artifacts/word2vec_algorithm_model_artifacts/model.tar.gz', 'model.tar.gz')
+
+    print('Deploying endpoint in local mode')
+    print(
+        'Note: if launching for the first time in local mode, container image download might take a few minutes to complete.')
+    model = SKLearnModel(
+        role=DUMMY_IAM_ROLE,
+        model_data='file://./model.tar.gz',
+        framework_version='0.23-1',
+        py_version='py3',
+        source_dir='code',
+        entry_point='inference.py'
+    )
+
+    print('Deploying endpoint in local mode')
+    predictor = model.deploy(initial_instance_count=1, instance_type='local')
+
+    payload = {"instances": ["king","queen"]}
+    predictor.serializer = JSONSerializer()
+    predictor.deserializer = JSONDeserializer()
+    predictions = predictor.predict(payload)
+    print(f"Predictions: {predictions}")
+
+    print('About to delete the endpoint to stop paying (if in cloud mode).')
+    predictor.delete_endpoint(predictor.endpoint_name)
+
+
+if __name__ == "__main__":
+    main()
