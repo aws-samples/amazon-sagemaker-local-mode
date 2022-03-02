72e5023e3d3ce2f98e8ac79773f7fb3fba9e7aa5
Eitan Sela <eitan.sela@gmail.com>
2022-03-02 15:13:23 +0200

    Added tensorflow_bring_your_own_california_housing_local_training_and_no_tfs_serving sample code

---
+++ tensorflow_bring_your_own_california_housing_local_training_and_no_tfs_serving/container/california_housing/predictor.py
@@ -0,0 +1,83 @@
+# This is the file that implements a flask server to do inferences. It's the file that you will modify to
+# implement the scoring for your own algorithm.
+
+from __future__ import print_function
+
+import io
+import json
+import os
+
+import flask
+import pandas as pd
+import tensorflow as tf
+
+prefix = '/opt/ml/'
+model_path = os.path.join(prefix, 'model')
+
+
+# A singleton for holding the model. This simply loads the model and holds it.
+# It has a predict function that does a prediction based on the model and the input data.
+
+class CaliforniaHousingService(object):
+    model = None  # Where we keep the model when it's loaded
+
+    @classmethod
+    def get_model(chs):
+        """Get the model object for this instance, loading it if it's not already loaded."""
+        if chs.model == None:
+            print('Loading Model')
+            chs.model = tf.keras.models.load_model('/opt/ml/model/1/')
+        return chs.model
+
+    @classmethod
+    def predict(chs, input):
+        """For the input, do the predictions and return them.
+
+        Args:
+            input (a pandas dataframe): The data on which to do the predictions. There will be
+                one prediction per row in the dataframe"""
+        model = chs.get_model()
+        return model.predict(input)
+
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    """Determine if the container is working and healthy. In this sample container, we declare
+    it healthy if we can load the model successfully."""
+    health = CaliforniaHousingService.get_model() is not None  # You can insert a health check here
+
+    status = 200 if health else 404
+    return flask.Response(response='\n', status=status, mimetype='application/json')
+
+
+@app.route('/invocations', methods=['POST'])
+def transformation():
+    """Do an inference on a single batch of data. In this sample server, we take data as JSON, convert
+    it to a pandas dataframe for internal use and then convert the predictions back to JSON (which really
+    just means one prediction per line, since there's a single column.
+    """
+    data = None
+
+    # Convert from JSON to Pandas
+    if flask.request.content_type == 'application/json':
+        data = flask.request.data.decode('utf-8')
+        instances = json.loads(data)["instances"]
+        print(f"instances: {instances}")
+        data = pd.DataFrame(data=instances)
+    else:
+        return flask.Response(response='This predictor only supports JSON data', status=415, mimetype='text/plain')
+
+    print('Invoked with {} records'.format(data.shape[0]))
+
+    # Do the prediction
+    predictions = CaliforniaHousingService.predict(data)
+
+    # Convert from numpy back to JSON
+    predictions_lists = predictions.tolist()
+    result = json.dumps({"predictions": predictions_lists})
+
+    return flask.Response(response=result, status=200, mimetype='application/json')
---
+++ tensorflow_bring_your_own_california_housing_local_training_and_no_tfs_serving/tensorflow_bring_your_own_california_housing_local_training_and_no_tfs_serving.py
@@ -0,0 +1,119 @@
+# This is a sample Python program that trains a BYOC TensorFlow model with no TFS, and then performs inference.
+# This implementation will work on your local computer.
+#
+# Prerequisites:
+#   1. Install required Python packages:
+#       pip install boto3 sagemaker pandas scikit-learn
+#       pip install 'sagemaker[local]'
+#   2. Docker Desktop has to be installed on your computer, and running.
+#   3. Open terminal and run the following commands:
+#       docker build  -t sagemaker-tensorflow2-no-tfs-local container/.
+########################################################################################################################
+
+import os
+
+import numpy as np
+import pandas as pd
+import sklearn.model_selection
+from sagemaker import Model, LocalSession, Predictor
+from sagemaker.deserializers import JSONDeserializer
+from sagemaker.estimator import Estimator
+from sagemaker.serializers import JSONSerializer
+from sklearn.datasets import *
+from sklearn.preprocessing import StandardScaler
+
+DUMMY_IAM_ROLE = 'arn:aws:iam::111111111111:role/service-role/AmazonSageMaker-ExecutionRole-20200101T000001'
+
+sagemaker_session = LocalSession()
+sagemaker_session.config = {'local': {'local_code': True}}
+
+
+def download_training_and_eval_data():
+    if os.path.isfile('./data/train/x_train.npy') and \
+            os.path.isfile('./data/test/x_test.npy') and \
+            os.path.isfile('./data/train/y_train.npy') and \
+            os.path.isfile('./data/test/y_test.npy'):
+        print('Training and evaluation datasets exist. Skipping Download')
+    else:
+        print('Downloading training and evaluation dataset')
+        data_dir = os.path.join(os.getcwd(), 'data')
+        os.makedirs(data_dir, exist_ok=True)
+
+        train_dir = os.path.join(os.getcwd(), 'data/train')
+        os.makedirs(train_dir, exist_ok=True)
+
+        test_dir = os.path.join(os.getcwd(), 'data/test')
+        os.makedirs(test_dir, exist_ok=True)
+
+        data_set = fetch_california_housing()
+
+        X = pd.DataFrame(data_set.data, columns=data_set.feature_names)
+        Y = pd.DataFrame(data_set.target)
+
+        # We partition the dataset into 2/3 training and 1/3 test set.
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.33)
+
+        scaler = StandardScaler()
+        scaler.fit(x_train)
+        x_train = scaler.transform(x_train)
+        x_test = scaler.transform(x_test)
+
+        np.save(os.path.join(train_dir, 'x_train.npy'), x_train)
+        np.save(os.path.join(test_dir, 'x_test.npy'), x_test)
+        np.save(os.path.join(train_dir, 'y_train.npy'), y_train)
+        np.save(os.path.join(test_dir, 'y_test.npy'), y_test)
+
+        print('Downloading completed')
+
+
+def do_inference_on_local_endpoint(predictor):
+    print(f'\nStarting Inference on endpoint (local).')
+
+    x_test = np.load('./data/test/x_test.npy')
+    y_test = np.load('./data/test/y_test.npy')
+
+    data = {"instances": x_test[:10]}
+    results = predictor.predict(data)['predictions']
+
+    flat_list = [float('%.1f' % (item)) for sublist in results for item in sublist]
+    print('predictions: \t{}'.format(np.array(flat_list)))
+    print('target values: \t{}'.format(y_test[:10].round(decimals=1)))
+
+
+def main():
+    download_training_and_eval_data()
+
+    image = 'sagemaker-tensorflow2-no-tfs-local'
+
+    role = DUMMY_IAM_ROLE
+    model_dir = 's3://aws-ml-blog/artifacts/tensorflow-script-mode-local-model-grpc-inference/model.tar.gz'
+
+    model = Model(
+        image_uri=image,
+        role=role,
+        model_data=model_dir,
+    )
+
+    print('Deploying endpoint in local mode')
+    print(
+        'Note: if launching for the first time in local mode, container image download might take a few minutes to complete.')
+
+    endpoint = model.deploy(
+        initial_instance_count=1,
+        instance_type='local',
+        endpoint_name="my-local-endpoint"
+    )
+
+    predictor = Predictor(endpoint_name="my-local-endpoint",
+                          sagemaker_session=sagemaker_session,
+                          serializer=JSONSerializer(),
+                          deserializer=JSONDeserializer())
+
+    do_inference_on_local_endpoint(predictor)
+
+    print('About to delete the endpoint to stop paying (if in cloud mode).')
+    predictor.delete_endpoint(predictor.endpoint_name)
+
+
+if __name__ == "__main__":
+    main()
