ff5edf0989c08d1d6ac095b62d12919fbc01ccde
Eitan Sela <eitan.sela@gmail.com>
2022-08-01 15:12:59 +0300

    Update inference.py

--- scikit_learn_script_mode_local_serving_multiple_models_with_one_invocation/code/inference.py
+++ scikit_learn_script_mode_local_serving_multiple_models_with_one_invocation/code/inference.py
@@ -1,10 +1,10 @@
-import logging
-import sys
-import numpy as np
-import os
-import joblib
 import glob
 import json
+import logging
+import sys
+
+import joblib
+import numpy as np
 
 JSON_CONTENT_TYPE = 'application/json'
 
@@ -15,7 +15,7 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 
 def input_fn(serialized_input_data, content_type):
     logger.info(f'input_fn - serialized_input_data: {serialized_input_data}, content_type: {content_type}')
-    
+
     if content_type == JSON_CONTENT_TYPE:
         payload = [np.array(json.loads(serialized_input_data))]
         return payload
@@ -36,7 +36,7 @@ def predict_fn(input_object, models_list):
         logger.info(f"predict_fn - result for model #{i}: {prediction}")
         if prediction > max_prediction:
             max_prediction = prediction
-        
+
     logger.info(f"returning response: {max_prediction}")
     return max_prediction
 
@@ -44,17 +44,17 @@ def predict_fn(input_object, models_list):
 # Load the model files from model_dir
 def model_fn(model_dir):
     logger.info(f'model_fn - model_dir: {model_dir}')
-    for file in glob.glob(model_dir+'/model_*', recursive=True):
+    for file in glob.glob(model_dir + '/model_*', recursive=True):
         print(file)
 
     logger.info(f"model_fn - loading models from: {model_dir}")
 
     models_list = []
-    for model_file in glob.glob(model_dir+'/model_*', recursive=True):
+    for model_file in glob.glob(model_dir + '/model_*', recursive=True):
         print(f'Loading model file: {model_file}')
         loaded_model = joblib.load(model_file)
         models_list.append(loaded_model)
 
     logger.info(f"model_fn - models_list length: {len(models_list)}")
-    
+
     return models_list
