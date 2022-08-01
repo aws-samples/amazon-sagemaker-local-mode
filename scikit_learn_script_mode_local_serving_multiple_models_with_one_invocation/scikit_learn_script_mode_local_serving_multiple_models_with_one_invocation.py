d48468bbe77523b0c7fb0a8f544eacc17cd5e3e1
Eitan Sela <eitan.sela@gmail.com>
2022-08-01 14:53:51 +0300

    Update scikit_learn_script_mode_local_serving_multiple_models_with_one_invocation.py

--- scikit_learn_script_mode_local_serving_multiple_models_with_one_invocation/scikit_learn_script_mode_local_serving_multiple_models_with_one_invocation.py
+++ scikit_learn_script_mode_local_serving_multiple_models_with_one_invocation/scikit_learn_script_mode_local_serving_multiple_models_with_one_invocation.py
@@ -16,6 +16,8 @@ from sagemaker import LocalSession
 from sagemaker.sklearn import SKLearnModel
 
 DUMMY_IAM_ROLE = 'arn:aws:iam::111111111111:role/service-role/AmazonSageMaker-ExecutionRole-20200101T000001'
+LOCAL_SESSION = LocalSession()
+LOCAL_SESSION.config={'local': {'local_code': True}} # Ensure full code locality, see: https://sagemaker.readthedocs.io/en/stable/overview.html#local-mode
 MAX_YEAR = 2022
 
 def gen_price(house):
@@ -52,10 +54,6 @@ def gen_random_house():
 
 
 def main():
-    sagemaker_session = LocalSession()
-    sagemaker_session.config = {'local': {'local_code': True}}
-
-    # For local training a dummy role will be sufficient
     role = DUMMY_IAM_ROLE
     model_dir = 's3://aws-ml-blog/artifacts/scikit_learn_serving_multiple_models_with_one_invocation/model.tar.gz'
 
@@ -65,7 +63,8 @@ def main():
         framework_version='0.23-1',
         py_version='py3',
         source_dir='code',
-        entry_point='inference.py'
+        entry_point='inference.py',
+        sagemaker_session=LOCAL_SESSION
     )
 
     print('Deploying endpoint in local mode')
